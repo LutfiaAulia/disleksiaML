8829b79c788df186b96a0b4cc55c1dddca6f65d8
LutfiaAulia <lutfiaaulia2@gmail.com>
2024-05-21 12:16:36 +0700

    :)

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def index():
-    return render_template ("index.html")
+    return render_template ("cek.html")
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
